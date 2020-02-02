f228aec4b4f1936fcdc2bc9a2de2e2c3444013dd
Karthik M <chynkm@gmail.com>
2020-02-02 18:01:51 +0530

    01-02-2020

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,10 +3,37 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("KM")
-	want := "Hello KM"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+    assertCorrectMessage := func(t *testing.T, got, want string) {
+        t.Helper()
+        if got != want {
+            t.Errorf("got %q want %q", got, want)
+        }
+    }
+
+    t.Run("say hello to people", func(t *testing.T) {
+        got := Hello("KM", "")
+        want := "Hello KM"
+
+        assertCorrectMessage(t, got, want)
+    })
+
+    t.Run("say 'Hello World' when an empty string is supplied", func(t *testing.T) {
+        got := Hello("", "")
+        want := "Hello World"
+
+        assertCorrectMessage(t, got, want)
+    })
+
+    t.Run("in Spanish", func(t *testing.T) {
+        got := Hello("KM", "Spanish")
+        want := "Hola KM"
+        assertCorrectMessage(t, got, want)
+    })
+
+    t.Run("in French", func(t *testing.T) {
+        got := Hello("KM", "French")
+        want := "Bonjour KM"
+        assertCorrectMessage(t, got, want)
+    })
 }
